e0f242fade721a9590f419cdb9ad8a97c81c7902
CJ <jayyang116@gmail.com>
2024-01-12 22:45:09 -0500

    Initial Commit

---
+++ CryptoWallet/CryptoWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoWalletApp.swift
+//  CryptoWallet
+//
+//  Created by CJ on 1/12/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
